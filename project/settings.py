36fc195f378d9abf6f3423b6e78e9b98aece7227
= <=>
2021-09-04 21:22:42 +0300

    add static and media settings

--- project/settings.py
+++ project/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+## MEDIA SETTINGS 
+MEDIA_ROOT = BASE_DIR / 'media'
+MDEIA_URL = '/media/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
